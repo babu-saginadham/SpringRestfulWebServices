267ecdc7c7a8a901eab964f15640b0571495373d
Babu Saginadham <babu.saginadham@gmail.com>
2021-02-21 22:33:11 +0530

    Add files via upload

---
+++ src/main/java/com/student/controller/AdminController.java
@@ -0,0 +1,31 @@
+package com.student.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/admin/")
+public class AdminController {
+
+	@RequestMapping(value = "/test", method = RequestMethod.GET)
+	public String test() {
+		String result="TEST URI ";  
+		return result;
+	}
+	
+	@RequestMapping(value = "/test2", method = RequestMethod.GET)
+	public String test2() {
+		String result="TEST2 URI ";  
+		return result;
+	}
+	
+	@RequestMapping(value = "/student/{name}", method = RequestMethod.GET)
+	public String hello(@PathVariable String name) {
+		String result="Hello "+name;  
+		return result;
+	}
+	
+}
